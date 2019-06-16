05e397d56b68e97f8419018cb5b0b624887d5436
Dom Henderson <dominic.henderson1@outlook.com>
2019-06-16 12:23:28 +0100

    Validated numeric inputs

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function getOperator ( validOperators ) {
     console.log('Enter the operator');
     var op = readline.prompt();
     while ( !validOperators.includes(op)) {
-        console.log('Operator \'op\' not recognised');
+        console.log('Operator \'' + op + '\' not recognised');
         console.log('Valid operators are \'' + validOperators.join('\', \'') +'\'' );
         console.log('Enter the operator');
         op = readline.prompt();
@@ -16,14 +16,23 @@ function getOperator ( validOperators ) {
     return op;
 }
 
+function requestValidNumber ( message ) {
+    console.log ( message );
+    var num = readline.prompt();
+    while ( isNaN ( +num ) ) {
+        console.log(num + ' is not a valid number');
+        console.log(message);
+        num = readline.prompt();
+    }
+    return Number(num);
+}
+
 function getArgCount ( op ) {
-    console.log('How many numbers do you want to ' + op + '?');
-    return Number(readline.prompt());
+    return requestValidNumber('How many numbers do you want to ' + op + '?');
 }
 
 function getOperand ( index ) {
-    console.log('Please enter number' + index + ':' );
-    return Number(readline.prompt());
+    return requestValidNumber('Please enter number' + index + ':' );
 }
 
 function getAllOperands ( count ) {
