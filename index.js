d18f2644ed0524215d7529bb09658c5f5dee1db5
Dom Henderson <dominic.henderson1@outlook.com>
2019-06-22 08:04:58 +0100

    Added vowel counting mode

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const readline = require('readline-sync');
 
+const ARITHMETIC_MODE = 1;
+const VOWEL_COUNTING_MODE = 2;
+
 function welcome () {
     console.log('Welcome to the calculator!');
 }
@@ -75,10 +78,58 @@ function performCalculation ( op, args ) {
     return args.reduce(func);
 }
 
-welcome();
-while ( true ) {
+function getCalculationMode() {
+    console.log('Which calculator mode do you want?');
+    console.log(`${ARITHMETIC_MODE}: arithmetic mode`);
+    console.log(`${VOWEL_COUNTING_MODE}: vowel counting mode`);
+    var mode = readline.prompt();
+    while ( mode != ARITHMETIC_MODE && mode != VOWEL_COUNTING_MODE ) {
+        console.log(`${mode} is not a valid mode`);
+        console.log('Which calculator mode do you want?');
+        console.log(`${ARITHMETIC_MODE}: arithmetic mode`);
+        console.log(`${VOWEL_COUNTING_MODE}: vowel counting mode`);
+        var mode = readline.prompt();
+    }
+    return mode;
+}
+
+function performOneArithmeticCalculation() {
     const op = getOperator(['+','-','*','/']);
     const num = getArgCount( op );
     const args = getAllOperands(num);
     console.log(`The result is ${performCalculation(op, args)}\n` );
+}
+
+function performOneVowelCountingCalculation() {
+    console.log('Please enter a string:');
+    const str = readline.prompt().toUpperCase();
+    var result = {
+        A:0, E:0, I:0, O:0, U:0, 
+        prettyPrint: function() {
+            console.log(`A: ${this.A}`);
+            console.log(`E: ${this.E}`);
+            console.log(`I: ${this.I}`);
+            console.log(`O: ${this.O}`);
+            console.log(`U: ${this.U}\n`);
+        }
+    };
+    for ( const c of str ) {
+        for ( var v in result ) {
+            if ( c == v ) {
+                result[c] += 1;
+            }
+        }
+    }
+    console.log('The vowel counts are:');
+    result.prettyPrint();
+}
+
+welcome();
+while ( true ) {
+    const calculationMode = getCalculationMode();
+    if ( calculationMode == ARITHMETIC_MODE ) {
+        performOneArithmeticCalculation();
+    } else if ( calculationMode == VOWEL_COUNTING_MODE ) {
+        performOneVowelCountingCalculation();
+    }
 }
